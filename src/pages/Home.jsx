880740a1c8f9472bca474a2b786b51b61ecc47b4
burademissie <156226117+burademissie@users.noreply.github.com>
2025-05-16 02:47:12 +0300

    s and S

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { StarBackground } from "../components/StarBackground";
 import { NavBar } from "../components/NavBar";
 import { HeroSection } from "../components/HeroSection";
 import { About } from "../components/About";
-import { Skills } from "../components/skills";
+import { Skills } from "../components/Skills";
 import { Projects } from "../components/Projects";
 import { Contact } from "../components/Contact";
 
