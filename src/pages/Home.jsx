05dbd81f3b1ffeee6088fee90d81a92988355fa6
burademissie <156226117+burademissie@users.noreply.github.com>
2025-05-16 03:13:36 +0300

    image path title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,12 @@ import { Skills } from "../components/Skills";
 import { Projects } from "../components/Projects";
 import { Contact } from "../components/Contact";
 
+
 export const Home = () => {
 
-    return <div className="min-h-screen bg-background text-forground overflow-x-hidden">
+    return (
+    
+    <div className="min-h-screen bg-background text-forground overflow-x-hidden">
         
         <ThemeToggle />
 
@@ -25,6 +28,8 @@ export const Home = () => {
             <Contact />
         </main>
 
-    </div>;
+    </div>
+    
+    )
 }
 
